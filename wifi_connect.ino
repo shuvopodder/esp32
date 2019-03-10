832b7058fa83caa853d185b3c7dba54b927a7c72
Shuvo Podder <38353778+shuvopodder@users.noreply.github.com>
2019-03-11 01:43:43 +0600

    Add files via upload

---
+++ wifi_connect.ino
@@ -0,0 +1,38 @@
+/*Shuvo Podder*/
+
+#include "WiFi.h"
+
+const char* ssid     = "ABC";         //wifi address
+const char* password = "12345";       //wifi password 
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Set WiFi to station mode and disconnect from an AP if it was previously connected
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+
+    Serial.println("Setup done");
+}
+
+void loop()
+{
+    
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    delay(5000);
+}
